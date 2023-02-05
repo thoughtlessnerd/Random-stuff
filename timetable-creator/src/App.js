142e44dfe600e3a9dc4ae4a8922ed026baf0768c
thoughtlessnerd <abhay.csgo001@gmail.com>
2023-02-05 11:56:47 +0530

    tooltip flicker and nth-check vulnerability fixed

--- timetable-creator/src/App.js
+++ timetable-creator/src/App.js
@@ -1,7 +1,36 @@
 import "./App.css";
+import { Chart } from "react-google-charts";
 
 function App() {
-    return <></>;
+    return (
+        <Chart
+            chartType="PieChart"
+            width="100%"
+            height="400px"
+            data={data}
+            options={options}
+        />
+    );
 }
+
+const data = [
+    ["Task", "Hours per Day", "tooltip"],
+    ["Work", 2, "2 hr"],
+    ["Eat", 2, "2 hr"],
+    ["Commute", 2, "2 hr"],
+    ["Watch TV", 2, "2 hr"],
+    ["Sleep", 4, "2 hr"], // CSS-style declaration
+];
+
+const options = {
+    pieHole: 0.8,
+    is3D: false,
+    legend: "none",
+    pieSliceText: "none",
+    tooltip: {
+        isHtml: true,
+        text: "value",
+    },
+};
 
 export default App;
